731544e60649034f7e87911710306494f07f8679
Dominic-Madeira <124389519+Dominic-Madeira@users.noreply.github.com>
2023-05-29 17:20:07 +0000

    done

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2023 Dominic M. All rights reserved
 //
 // Created by: Dominic M.
-// Created on: Sep 2020
+// Created on: May 2023
 // This file contains the JS functions for index.html
 
 "use strict"
