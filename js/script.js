a878c2ceeee022966266b584090aa87fb271ee61
Dominic-Madeira <124389519+Dominic-Madeira@users.noreply.github.com>
2023-05-29 16:55:01 +0000

    start

--- js/script.js
+++ js/script.js
@@ -1,5 +1,23 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2023 Dominic M. All rights reserved
 //
-// Created by: Mr. Coxall
+// Created by: Dominic M.
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+
+"use strict"
+
+window.onload = function() {
+  // this calculates volume of a sphere
+
+  const params = new URLSearchParams(document.location.search)
+
+  // input
+  const radius = params.get('r')
+
+  // process
+  const volume = ((4 / 3) * Math.PI) * radius ** 3
+
+  // output
+  document.getElementById('radius').innerHTML = "The radius is: " + radius + ' mm'
+  document.getElementById('volume').innerHTML = 'The volume is: ' + volume + ' mm³'
+}
